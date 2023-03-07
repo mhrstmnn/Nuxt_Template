848d06fd7ee85b239d2a9c7ab3d1c2d6361891bb
Michael Horstmann <git@hrstmnn.de>
2023-03-07 13:30:38 +0100

    added head config and css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      htmlAttrs: { lang: 'de' },
+      // title: '',
+      meta: [
+        // { name: 'description', content: '' },
+        // { name: 'keywords', content: '' },
+        { name: 'author', content: 'Michael Horstmann' },
+      ],
+    },
+  },
+  css: ['normalize.css', '@/assets/css/main.css'],
 })
