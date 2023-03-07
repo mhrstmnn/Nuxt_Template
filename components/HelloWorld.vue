70ddb08e1ec3e642a93e7204f259fde83be1681e
Michael Horstmann <git@hrstmnn.de>
2023-03-07 13:46:16 +0100

    example page and component

---
+++ components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <p>Hello {{ props.name ?? 'World' }}</p>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+useHead({ title: 'Index' })
+</script>
+
+<template>
+  <HelloWorld name="Michael" />
+</template>
