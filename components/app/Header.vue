9c17ebf31a78e2596897d58660327a33518eb57e
Michael Horstmann <git@hrstmnn.de>
2023-10-24 23:40:14 +0200

    refactored Header

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,11 +1,15 @@
 <template>
   <header>
-    <h1><NuxtLink to="/">Nuxt Starter</NuxtLink></h1>
+    <h1 class="mt-0"><NuxtLink to="/">Nuxt Starter</NuxtLink></h1>
   </header>
 </template>
 
 <style scoped>
-header > h1 > a {
-  color: unset;
+header > h1 {
+  color: var(--primary-color);
+
+  & > a {
+    color: unset;
+  }
 }
 </style>
