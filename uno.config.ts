1854065bc89ae10c3fe5917d935ddedc605333f6
Michael Horstmann <git@hrstmnn.de>
2023-10-25 00:32:10 +0200

    added typography preset for uno css

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno, presetTypography } from 'unocss'
 
 export default defineConfig({
   // UnoCSS options
+  presets: [
+    presetUno(), // required
+    presetTypography(),
+  ],
 })
