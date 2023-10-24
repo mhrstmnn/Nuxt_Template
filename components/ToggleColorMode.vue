e379383ce8b4f28b08208e7a001066fb6a3920cd
Michael Horstmann <git@hrstmnn.de>
2023-10-24 22:58:12 +0200

    added toggle color mode component

---
+++ components/ToggleColorMode.vue
@@ -0,0 +1,8 @@
+<template>
+  <select v-model="$colorMode.preference">
+    <option value="system">System</option>
+    <option value="light">Hell</option>
+    <option value="dark">Dunkel</option>
+    <option value="sepia">Sepia</option>
+  </select>
+</template>
