81532946336436f0cd021e35fe0c85ba4f466e9a
Michael Horstmann <git@hrstmnn.de>
2023-10-25 02:36:14 +0200

    refactored Footer

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -3,31 +3,25 @@ import Package from '../../package.json'
 </script>
 
 <template>
-  <footer>
-    Version: {{ Package.version }}
-    <br />
-    Theme: <ToggleColorMode />
-    <br />
-    <br />
-    &copy; {{ new Date().getFullYear() }}
-    <NuxtLink to="https://links.hrstmnn.de" target="_blank"
-      >Michael Horstmann</NuxtLink
-    >
-    <br />
-    <NuxtLink to="https://hrstmnn.de/impressum/" target="_blank"
-      >Impressum</NuxtLink
-    >
-    und
-    <NuxtLink to="https://hrstmnn.de/datenschutz/" target="_blank"
-      >Datenschutz</NuxtLink
-    >
+  <footer class="mt-8 xl:mt-10">
+    <p class="mb-0 font-light leading-relaxed">
+      Version: {{ Package.version }}
+      <br />
+      Theme: <ToggleColorMode class="font-light" />
+      <br />
+      <br />
+      &copy; {{ new Date().getFullYear() }}
+      <NuxtLink to="https://links.hrstmnn.de" target="_blank"
+        >Michael Horstmann</NuxtLink
+      >
+      <br />
+      <NuxtLink to="https://hrstmnn.de/impressum/" target="_blank"
+        >Impressum</NuxtLink
+      >
+      und
+      <NuxtLink to="https://hrstmnn.de/datenschutz/" target="_blank"
+        >Datenschutz</NuxtLink
+      >
+    </p>
   </footer>
 </template>
-
-<style scoped>
-footer {
-  margin-top: 2rem;
-  font-size: 1rem;
-  filter: invert(10%);
-}
-</style>
